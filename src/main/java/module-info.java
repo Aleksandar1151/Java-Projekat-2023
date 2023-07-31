e36100cf284ab4a616196279d1a5c814feb8e8ad
Aleksandar <jovanovic.a.996@gmail.com>
2023-07-31 17:15:32 +0200

    Update 1 31.7.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module granicni_prelaz.javaprojekat2023 {
 
     opens granicni_prelaz.javaprojekat2023 to javafx.fxml;
     exports granicni_prelaz.javaprojekat2023;
+    exports granicni_prelaz.javaprojekat2023.Kontroleri;
+    opens granicni_prelaz.javaprojekat2023.Kontroleri to javafx.fxml;
 }
